19eb09cfb795c28b8e32f1151d8fb83efea4eeb0
byoungkwan <purple0seal@gmail.com>
2022-03-18 18:09:05 +0900

    Add broadcast term

--- app/components-react/shared/inputs/RadioInput.tsx
+++ app/components-react/shared/inputs/RadioInput.tsx
@@ -4,29 +4,35 @@ import InputWrapper from './InputWrapper';
 import { Radio, Space } from 'antd';
 
 type TRadioInputProps = TSlobsInputProps<
-  { options: { value: string; label: string; description?: string }[]; buttons?: boolean },
+  {
+    options: { value: string; label: string; description?: string; disabled?: boolean }[];
+    buttons?: boolean;
+    direction?: 'vertical' | 'horizontal';
+    nowrap?: boolean;
+  },
   string,
   {}
 >;
 
 export const RadioInput = InputComponent((p: TRadioInputProps) => {
   return (
-    <InputWrapper label={p.label}>
+    <InputWrapper label={p.label} nowrap={p.nowrap}>
       {p.buttons && (
         <Radio.Group
           value={p.value}
           onChange={e => p.onChange && p.onChange(e.target.value)}
           options={p.options}
+          disabled={p.disabled}
           optionType="button"
           buttonStyle="solid"
         />
       )}
       {!p.buttons && (
         <Radio.Group value={p.value} onChange={e => p.onChange && p.onChange(e.target.value)}>
-          <Space direction="vertical">
+          <Space direction={p.direction || 'vertical'}>
             {p.options.map(option => {
               return (
-                <Radio key={option.value} value={option.value}>
+                <Radio key={option.value} value={option.value} disabled={p.disabled}>
                   {option.label}
                   {option.description && <br />}
                   {option.description && <span style={{ fontSize: 12 }}>{option.description}</span>}
